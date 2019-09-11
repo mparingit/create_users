67db3f76116fb6b006b60ec4ebc8110d98dc0983
hpparingit <melvin.bau.paringit@dxc.com>
2019-09-11 14:52:36 +0000

    changes

--- roles/create_users/create_users/jenkins.sh
+++
@@ -1,9 +0,0 @@
-sudo docker run \
-  -u root \
-  -d \
-  -p 8080:8080 \
-  -p 50000:50000 \
-  -v jenkins-data:/var/jenkins_home \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  jenkinsci/blueocean
-
